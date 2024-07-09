483e46f0068480e674ff305a79eeff30d6253726
kathan07 <thakkarkathan1904@gmail.com>
2024-07-09 22:04:40 +0530

    mailto func

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
             <div className="text-center tracking-tighter">
                 <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className="my-4">{CONTACT.phoneNo}</motion.p>
                 <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className='my-4'>
-                    <a href="#" className="border-b">
+                    <a href={`mailto:${CONTACT.email}`} target='_blank' className="border-b">
                         {CONTACT.email}
                     </a>
                 </motion.p>
